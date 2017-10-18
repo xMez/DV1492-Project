324eb26786e594d88c9fc7027ac801233b9b5667
Mez <exorkistiz@hotmail.com>
2017-10-18 21:54:46 +0200

    Laying out framework functions

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,7 +1,8 @@
 #include "filesystem.h"
 
 FileSystem::FileSystem() {
-
+    this->root = Folder("/");
+    this->currDir = &this->root;
 }
 
 FileSystem::~FileSystem() {
@@ -10,3 +11,11 @@ FileSystem::~FileSystem() {
 
 
 /* Please insert your code */
+
+void FileSystem::createFolder(std::string name) {
+    this->currDir->folders.push_back(Folder(name, this->currDir));
+}
+
+void FileSystem::removeFolder(std::string name) {
+    
+}
